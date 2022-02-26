6c27c2460cb1641115691b83b2c3bab5f5ea8295
Yisake Beyene <tadeleyisake@gmail.com>
2022-02-25 21:50:46 -0500

    Added the main table view and loaded the titles of the movies in it.

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -7,12 +7,17 @@
 
 import UIKit
 
-class MoviesViewController: UIViewController {
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
 
+    @IBOutlet weak var tableView: UITableView!
     var movies = [[String:Any]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view.
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
@@ -26,6 +31,9 @@ class MoviesViewController: UIViewController {
                     let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                     
                     self.movies = dataDictionary["results"] as! [[String:Any]]
+                
+                self.tableView.reloadData()
+                
                     print(dataDictionary)
                     // TODO: Get the array of movies
                     // TODO: Store the movies in a property to use elsewhere
@@ -35,6 +43,23 @@ class MoviesViewController: UIViewController {
         }
         task.resume()
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return  movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        
+        cell.textLabel?.text =  title
+        
+        return cell
+        
+    }
+    
 
 
 }
