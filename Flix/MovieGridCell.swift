20798ad0d738b0b416ae297a02a52fa70ce60aae
Yisake Beyene <tadeleyisake@gmail.com>
2022-03-02 22:14:03 -0500

    Finished the GridView UI with three columns

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Isaac Tadele Beyene on 3/2/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
